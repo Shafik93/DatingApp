7956170f58cb9457e398696cf2038fe53218e497
Shafik <shafik.oudeh13@gmail.com>
2019-12-31 11:28:19 -0500

    Finishing up the likes list

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient, HttpParams, HttpHeaderResponse } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpParams
+} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../_models/User';
 import { PaginatedResult, Pagination } from '../_models/Pagination';
@@ -14,10 +17,13 @@ export class UserService {
 
   constructor(private http: HttpClient) {}
 
-  getUsers(page?, itemsPerPage?, userParams?): Observable<PaginatedResult<User[]>> {
-    const paginatedResult: PaginatedResult<User[]> = new PaginatedResult<
-      User[]
-    >();
+  getUsers(
+    page?,
+    itemsPerPage?,
+    userParams?,
+    likesParams?
+  ): Observable<PaginatedResult<User[]>> {
+    const paginatedResult: PaginatedResult<User[]> = new PaginatedResult<User[]>();
     let params = new HttpParams();
 
     if (page != null && itemsPerPage != null) {
@@ -25,11 +31,19 @@ export class UserService {
       params = params.append('pageSize', itemsPerPage);
     }
 
-    if(userParams != null) {
+    if (userParams != null) {
       params = params.append('minAge', userParams.minAge);
       params = params.append('maxAge', userParams.maxAge);
       params = params.append('gender', userParams.gender);
       params = params.append('orderBy', userParams.orderBy);
+    }
+
+    if (likesParams === 'Likers') {
+      params = params.append('likers', 'true');
+    }
+
+    if (likesParams === 'Likees') {
+      params = params.append('likees', 'true');
     }
 
     return this.http
@@ -67,6 +81,9 @@ export class UserService {
   }
 
   sendLike(userid: number, recipientId: number) {
-    return this.http.post(this.baseUrl + 'users/' + userid + '/like/' + recipientId, {});
+    return this.http.post(
+      this.baseUrl + 'users/' + userid + '/like/' + recipientId,
+      {}
+    );
   }
 }
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import { ListResolver } from './_resolvers/list.resolver';
 
 
 export function tokenGetter(){
@@ -86,6 +87,7 @@ export class CustomHammerConfig extends HammerGestureConfig  {
       MemberListResolver,
       { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig }, //problem with gallery library being used with Angular
       MemberEditResolver,
+      ListResolver,
       PreventUnsavedChanges
    ],
    bootstrap: [
